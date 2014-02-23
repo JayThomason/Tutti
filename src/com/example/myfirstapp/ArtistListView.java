ad73ab0b976eced1265e5fb19efea1edf855be76
JayThomason <jayt92@stanford.edu>
2014-02-22 17:33:45 -0800

    init commit.

---
+++ src/com/example/myfirstapp/ArtistListView.java
@@ -0,0 +1,67 @@
+package com.example.myfirstapp;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class ArtistListView extends Activity {
+    ListView listView;
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_android_example);
+        
+        ArrayList<String> artists = new ArrayList<String>();
+
+        Cursor cursor = getContentResolver().query(
+        	    MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI, 
+        	    null, 
+        	    null, 
+        	    null, 
+        	    MediaStore.Audio.Artists.ARTIST + " ASC");
+        
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Artists.ARTIST));
+            artists.add(0, title);
+        }
+                
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+          android.R.layout.simple_list_item_1, android.R.id.text1, artists);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter); 
+        
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView<?> parent, View view,
+            		  int position, long id) {
+            	  // ListView Clicked item index
+            	  int itemPosition     = position;         
+            	  // ListView Clicked item value
+            	  String  itemValue    = (String) listView.getItemAtPosition(position);     
+            	  startActivity(new Intent(this, ArtistAlbumView.class));
+              }
+
+        }); 
+    }
+
+}
---
+++ src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Intent intent = new Intent(this, ArtistListView.class);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
