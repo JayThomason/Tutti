9a2ceb6ec3a92beb26ec7748c551c8aa6d83d924
JayThomason <jayt92@stanford.edu>
2014-03-06 23:09:54 -0800

    Globals now keeps track of the currently selected artist, album, and song. This removes the need to pass strings around between music browsing activities.

--- src/com/stanford/tutti/Globals.java
+++ src/com/stanford/tutti/Globals.java
@@ -9,6 +9,9 @@ public class Globals extends Application {
 	private ArrayList<Artist> artistList = new ArrayList<Artist>();
 	private ArrayList<Album> albumList = new ArrayList<Album>();
 	private HashMap<String, Artist> artistMap = new HashMap<String, Artist>();
+	private Song currentSong;
+	private Artist currentArtist;
+	private Album currentAlbum;
 		
 	public ArrayList<Artist> getArtistList() {
 		return artistList;
@@ -29,5 +32,29 @@ public class Globals extends Application {
 	
 	public void addAlbum(Album album) {
 		albumList.add(album);
+	}
+	
+	public Song getCurrentSong() {
+		return currentSong;
+	}
+	
+	public void setCurrentSong(Song song) {
+		this.currentSong = song;
+	}
+	
+	public Artist getCurrentArtist() {
+		return this.currentArtist;
+	}
+	
+	public void setCurrentArtist(Artist artist) {
+		this.currentArtist = artist;
+	}
+	
+	public Album getCurrentAlbum() {
+		return this.currentAlbum;
+	}
+	
+	public void setCurrentAlbum(Album album) {
+		this.currentAlbum = album;
 	}
 }
