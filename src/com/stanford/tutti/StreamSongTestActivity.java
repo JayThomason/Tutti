3662ed57ab54fa19e66ef53a454e570d09ded149
JayThomason <jayt92@stanford.edu>
2014-03-17 15:58:08 -0700

    removing unnecessary imports

--- src/com/stanford/tutti/StreamSongTestActivity.java
+++ src/com/stanford/tutti/StreamSongTestActivity.java
@@ -1,13 +1,6 @@
 package com.stanford.tutti;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Map;
 
 import android.app.ActionBar;
