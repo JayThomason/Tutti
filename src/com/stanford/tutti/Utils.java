a6d56e8fd982ac299e35574fc14fb66ebc6ab1a0
JayThomason <jayt92@stanford.edu>
2014-03-17 21:23:32 -0700

    Created a utils class for static methods not tied to other classes. Added method to build a unique key string for a song.

---
+++ src/com/stanford/tutti/Utils.java
@@ -0,0 +1,27 @@
+package com.stanford.tutti;
+
+/*
+ * Static utility methods not associated with a specific class. 
+ */
+public class Utils {
+	
+	/*
+	 * Builds a unique key from a song. We claim that a song can be uniquely
+	 * identified by its artist, album, and title.
+	 * 
+	 * @param Song song
+	 * 
+	 * @return String
+	 */
+	public static String getUniqueKeyForSong(Song song) {
+		StringBuilder keyBuilder = new StringBuilder("");
+		Artist artist = song.getArtist();
+		if (artist != null)
+			keyBuilder.append(artist.getName());
+		Album album = song.getAlbum();
+		if (album != null) 
+			keyBuilder.append(album.getTitle());
+		keyBuilder.append(song.getTitle());
+		return keyBuilder.toString(); 
+	}
+}
