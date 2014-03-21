ecd802591c53fa4378199a8503b344faafb9ea83
JayThomason <jayt92@stanford.edu>
2014-03-20 20:50:41 -0700

    removed unused imports

--- src/com/stanford/tutti/ViewJamActivity.java
+++ src/com/stanford/tutti/ViewJamActivity.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
