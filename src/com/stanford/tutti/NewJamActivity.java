331c621dee43dddb2948afba8317d645f9766d0a
JayThomason <jayt92@stanford.edu>
2014-03-20 21:04:41 -0700

    Refactored NewJamActivity.java. Might still want to rename file/class later.

--- src/com/stanford/tutti/NewJamActivity.java
+++ src/com/stanford/tutti/NewJamActivity.java
@@ -1,25 +1,11 @@
 package com.stanford.tutti;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.*;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -28,120 +14,82 @@ import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.ExpandableListView;
 import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupCollapseListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.Toast;
 
-
-
+/*
+ * The NewJam Activity is started when a new jam is created on a master
+ * phone or a jam is connected to from a client phone (thus it is probably
+ * a bit of a misnomer for now). 
+ * It displays a artist/song dropdown list. When a song is clicked on, it is
+ * added to the jam playlist on both phones and, if a song is not currently
+ * playing, it begins playing from the master.
+ */
 public class NewJamActivity extends Activity {
-
-	ExpandableListAdapter listAdapter;
-	ExpandableListView expListView;
-	List<String> listDataHeader;
-	HashMap<String, List<String>> listDataChild;
-
+	private ExpandableListAdapter listAdapter;
+	private ExpandableListView expListView;
+	private List<String> listDataHeader;
+	private HashMap<String, List<String>> listDataChild;
 	private final int PORT = 1234;
 	private Server server;
 	private Globals g; 
 	private Handler handler;
-	
 	private boolean master; 
-
 	// We should really be building this up as a global
 	HashMap<String, Song> songMap; 
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_jam);
-		// Show the Up button in the action bar.
 		setupActionBar();
-
-		Bundle b = getIntent().getExtras();
-		if (b != null && b.containsKey("host")) {
-			int value = b.getInt("host");
-			master = (value == 1); 
-		} else {
-			master = false; 
-		}
-		
+		getMasterBoolFromBundle();
 		g = (Globals) getApplication(); 
 		g.jam.setMaster(master); 
-
-		// Show the unique code for "join jam" requests
+		// Show the unique code (ip) for "join jam" requests
 		EditText editText = (EditText) this.findViewById(R.id.ip_address);
 		editText.setText("Your Jam ID is: " + g.getIpAddr());
-
 		// get the listview
 		expListView = (ExpandableListView) findViewById(R.id.listView1);
-
 		// preparing list data
 		prepareListData();
-
 		listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-
 		// setting list adapter
 		expListView.setAdapter(listAdapter);
-
-		expListView.setOnChildClickListener(new OnChildClickListener() {
-
-			@Override
-			public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-
-				String songName = listDataChild.get(listDataHeader.get(groupPosition)).get(childPosition); 
-				Song song = songMap.get(songName); 
-				Globals g = (Globals) getApplication();
-				g.jam.addSong(song); 
-				Toast.makeText(
-						getApplicationContext(),
-						listDataHeader.get(groupPosition)
-						+ " : "
-						+ songName 
-						+ " added to Jam", Toast.LENGTH_SHORT)
-						.show();                
-
-				if (master) {
-					if (g.jam.getCurrentSong() == null) {
-						g.jam.setCurrentSong(song);
-						g.jam.playCurrentSong();
-					}          
-				} 
-				
-				// Send a message to the other phone to add the song to its version of the jam
-				new PassMessageThread(g.jam.getOtherIP(), "/jam/add/" + Integer.toString(song.hashCode())).start(); 
-
-				return false;
-			}
-		});
-
+		setItemOnClickListener();
+		setUpHandler();
+		setUpServer();
+	}
+	
+	/*
+	 * Initializes the handler. The handler is used to receive messages from
+	 * the server and to update the UI accordingly.
+	 */
+	private void setUpHandler() {
 		handler = new Handler() {
 			@Override
 			public void handleMessage(Message msg) {
 				/*
-				 * When we get a message from the server that we have new non-local music, we can
-				 * update the list-view for the library.
+				 * When we get a message from another phone that we have new
+				 * non-local music, we can update the list-view for the library.
 				 */
 				if (msg.what == 0) {
-					System.out.println("ATTEMPTING TO UPDATE LIST VIEWWWWWWW\n\n\n");
 					prepareListData();
-					listAdapter = new ExpandableListAdapter(getBaseContext(), listDataHeader, listDataChild);
+					listAdapter = new ExpandableListAdapter(
+							getBaseContext(), listDataHeader, listDataChild);
 					expListView.setAdapter(listAdapter);
 				}
 				super.handleMessage(msg);
 			}
-		};
-		
+		};		
+	}
+	
+	/*
+	 * Starts the embedded NanoHttpd server.
+	 */
+	private void setUpServer() {
 		server = new Server(PORT, g, handler);
 		try {
 			server.start();
@@ -151,13 +99,57 @@ public class NewJamActivity extends Activity {
 	}
 
 	/*
-	 * Set up the nested/expandable ListView
+	 * Sets the onClickListener for the items (songs) in the artist/song 
+	 * drop down list.
+	 */
+	private void setItemOnClickListener() {
+		expListView.setOnChildClickListener(new OnChildClickListener() {
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v, 
+					int groupPosition, int childPosition, long id) {
+				String songName = listDataChild.get(
+						listDataHeader.get(groupPosition)).get(childPosition); 
+				Song song = songMap.get(songName); 
+				Globals g = (Globals) getApplication();
+				g.jam.addSong(song); 
+				Toast.makeText(getApplicationContext(),
+						listDataHeader.get(groupPosition)
+						+ " : " + songName 
+						+ " added to Jam", Toast.LENGTH_SHORT).show();                
+				if (master) {
+					if (g.jam.getCurrentSong() == null) {
+						g.jam.setCurrentSong(song);
+						g.jam.playCurrentSong();
+					}          
+				} 
+				new PassMessageThread(g.jam.getOtherIP(), 
+						"/jam/add/" + Integer.toString(song.hashCode())).start(); 
+				return false;
+			}
+		});
+	}
+	
+	/*
+	 * Initializes the master boolean to either true or false depending on
+	 * whether the activity was started with a host variable in the 
+	 */
+	private void getMasterBoolFromBundle() {
+		Bundle b = getIntent().getExtras();
+		if (b != null && b.containsKey("host")) {
+			int value = b.getInt("host");
+			master = (value == 1); 
+		} else {
+			master = false; 
+		}
+	}
+
+	/*
+	 * Set up the nested/expandable ListView of artist/songs.
 	 */
 	private void prepareListData() {
 		listDataHeader = new ArrayList<String>();
 		listDataChild = new HashMap<String, List<String>>();
 		songMap = new HashMap<String, Song>(); 
-
 		Globals g = (Globals) getApplication();
 		ArrayList<Artist> artists = g.getArtistList();
 
@@ -175,8 +167,6 @@ public class NewJamActivity extends Activity {
 			}
 			listDataChild.put(listDataHeader.get(i), songs);
 		}
-
-
 	}
 
 
