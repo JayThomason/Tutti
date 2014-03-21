e92da7a8c58b0f93cfa61dfb2eca54e1d2d348a1
JayThomason <jayt92@stanford.edu>
2014-03-20 21:24:22 -0700

    Refactored PassMessageThread to actually use the ip address provided and not rely on Globals. Commented.

--- src/com/stanford/tutti/PassMessageThread.java
+++ src/com/stanford/tutti/PassMessageThread.java
@@ -1,40 +1,39 @@
 package com.stanford.tutti;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import com.stanford.tutti.NanoHTTPD.Response.Status;
-
-import android.net.Uri;
-
+/*
+ * Creates a networking thread which sends an HTTP GET request
+ * to the path at the specified ip address.
+ */
 class PassMessageThread extends Thread {
-
 	private String ipAddress;
 	private final int PORT = 1234;
-	private Globals g; 
 	private String path; 
 
+	/*
+	 * Creates a new PassMessageThread for the ip/path pair.
+	 * Note: the path should begin with a '/' (it must be
+	 * an absolute path on the server).
+	 */
 	public PassMessageThread(String ip, String path) {
-		ipAddress = ip; 
-		g = (Globals) Globals.getAppContext(); 
+		ipAddress = ip;
 		this.path = path; 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
 	public void run() {
 		HttpClient httpClient = new DefaultHttpClient();
-		String uri = "http://" + g.jam.getOtherIP() + ":" + PORT + path;
+		String uri = "http://" + ipAddress + ":" + PORT + path;
 		HttpGet get = new HttpGet(uri.toString());
 		try {
 			// Right now we're not doing anything with this response. 
