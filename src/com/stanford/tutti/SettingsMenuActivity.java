25b88a474a01d5303ef363b2b3895ec2b5f9a1a5
unknown <hwray@stanford.edu>
2014-05-12 15:55:08 -0700

    Added OnSharedPreferenceChangeListener to SettingsMenuActivity to listen for and react to changes in username and other preferences.

--- src/com/stanford/tutti/SettingsMenuActivity.java
+++ src/com/stanford/tutti/SettingsMenuActivity.java
@@ -1,10 +1,14 @@
 package com.stanford.tutti;
  
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
  
 public class SettingsMenuActivity extends PreferenceActivity {
  
+	private SharedPreferences.OnSharedPreferenceChangeListener listener; 
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,6 +16,18 @@ public class SettingsMenuActivity extends PreferenceActivity {
         getActionBar().hide(); 
  
         addPreferencesFromResource(R.xml.settings);
- 
+        
+        SharedPreferences prefs = 
+        	    PreferenceManager.getDefaultSharedPreferences(this);
+        
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        		if (key.equals("prefUsername")) {
+        			
+        		} 
+        	}
+        };
+
+        prefs.registerOnSharedPreferenceChangeListener(listener);	
     }
 }
