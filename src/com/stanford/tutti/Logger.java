59c3593f894bb441a7b43eadb14a59590c687332
JayThomason <jayt92@stanford.edu>
2014-05-26 23:53:05 -0700

    Implemented updateUsers method in Logger.

--- src/com/stanford/tutti/Logger.java
+++ src/com/stanford/tutti/Logger.java
@@ -1,15 +1,25 @@
 package com.stanford.tutti;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/*
+ * Logs information about the jam such as elapsed time spent in jam, number 
+ * of unique users in jam, and the max number of songs in the jam to a local
+ * database. Once per day the database is serialized as JSON and sent to a
+ * centralized server.
+ */
 public class Logger {
 	private long currentJamId;
 	private Globals g;
 	private AtomicBoolean shouldUpdateTimestamp;
+	private Set<String> ipAddrSet;
 	
 	public Logger(Globals g) {
 		this.g = g;
 		shouldUpdateTimestamp = new AtomicBoolean(false);
+		ipAddrSet = new HashSet<String>();
 	}
 	
 	/*
@@ -38,6 +48,7 @@ public class Logger {
 						}
 					}
 					else {
+						System.out.println("Stopping updating timestamp for jam log");
 						break;
 					}
 				}
@@ -48,7 +59,7 @@ public class Logger {
 	}
 	
 	/*
-	 * Stops logging the 
+	 * Stops updating the timestamp for the current jam.
 	 */
 	public void endCurrentJam() {
 		shouldUpdateTimestamp.set(false);
@@ -61,6 +72,17 @@ public class Logger {
 		int numRowsUpdated = g.db.updateNumSongs(currentJamId);
 		if (numRowsUpdated != 1) {
 			System.out.println("Error updating number of songs in the jam log table.");
+		}
+	}
+	
+	/*
+	 * Updates the number of users in the jam if the user's ip address has not been
+	 * seen before.
+	 */
+	public void updateUsers(String ipAddr) {
+		if (!ipAddrSet.contains(ipAddr)) {
+			ipAddrSet.add(ipAddr);
+			g.db.incrementJamNumUsers(currentJamId);
 		}
 	}
 
