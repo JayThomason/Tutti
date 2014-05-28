2bb8f7844a75ed8fc736fab23717e7db34c23aa4
JayThomason <jayt92@stanford.edu>
2014-05-28 02:16:42 -0700

    Set app to log to server once every ~6 hours. Track whether logging alarm has been set with shared pref bool.

--- src/com/stanford/tutti/LoggerAlarmReceiver.java
+++ src/com/stanford/tutti/LoggerAlarmReceiver.java
@@ -16,11 +16,14 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 public class LoggerAlarmReceiver extends WakefulBroadcastReceiver {
 	private AlarmManager alarmManager;
 	private PendingIntent alarmIntent;
+	private static final String ALARM_SET_FLAG = "alarmSet";
 
 	@Override
 	public void onReceive(Context context, final Intent intent) {
@@ -35,30 +38,28 @@ public class LoggerAlarmReceiver extends WakefulBroadcastReceiver {
 			if (jsonJamLog == null || jsonJamLog.getInt("numEntries") <= 0) {
 				System.out.println("jsonJamLog is null or empty -- not logging to server");
 				System.out.println("jsonJamLog: " + jsonJamLog);
-				this.completeWakefulIntent(intent);
+				LoggerAlarmReceiver.completeWakefulIntent(intent);
 				return;
 			}
 
 			String serverUrl = context.getString(R.string.ec2_server);
 			String logUrl = "http://" + serverUrl + "/log";
-
 			AsyncHttpClient client = new AsyncHttpClient();
-			
 	        StringEntity entity = new StringEntity(jsonJamLog.toString());
-	        final WakefulBroadcastReceiver ref = this;
+	        
 	        client.post(context, logUrl, entity, "application/json",
 	            new AsyncHttpResponseHandler() {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 						System.out.println("Logged jam data to server");
 						System.out.println("deleted " + g.db.deleteLogData() + " jams from log");
-						ref.completeWakefulIntent(intent);
+						LoggerAlarmReceiver.completeWakefulIntent(intent);
 					}
 		
 					@Override
 					public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
 						System.out.println("Failed to log jam data to server");
-						ref.completeWakefulIntent(intent);
+						LoggerAlarmReceiver.completeWakefulIntent(intent);
 					}		
 				});
 		} catch (JSONException e) {
@@ -71,33 +72,31 @@ public class LoggerAlarmReceiver extends WakefulBroadcastReceiver {
 	}
 
 	public void setAlarm(Context context) {
-		
 		System.out.println("set alarm called in logger alarm");
 		alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 		Intent intent = new Intent(context, LoggerAlarmReceiver.class);
-		alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+		alarmIntent = PendingIntent.getBroadcast(context, 1234, intent, 0);
+		
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
 
 		// only set alarm if not already set!
-		if (PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_NO_CREATE) != null) {
-			System.out.println("setting alarm for real");
-
+		// note: this call still returns not null even if the alarm has been canceled -- bad for testing
+		if (!preferences.getBoolean(ALARM_SET_FLAG,  false)) {
 			Random random = new Random();
-
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTimeInMillis(System.currentTimeMillis());
-			// Set the alarm's trigger time to 8:30 a.m.
-			//calendar.set(Calendar.HOUR_OF_DAY, random.nextInt(24));
-			//calendar.set(Calendar.MINUTE, random.nextInt(60));
-			//calendar.set(Calendar.MINUTE, random.nextInt(60));
+			// Set the alarm's trigger to be a random time during the next ~6 hours, and repeat every ~6 hours
+			calendar.add(Calendar.HOUR_OF_DAY, random.nextInt(6));
+			calendar.add(Calendar.MINUTE, random.nextInt(60));
+			
+			System.out.println("Setting alarm trigger for logger: " + calendar);
 
-			// for testing, set in 1 second and repeat every 10 seconds
-			alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, 10000, alarmIntent);
-			alarmManager.cancel(alarmIntent);
-			//alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-			//        calendar.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, alarmIntent);
+			//alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, 5000, alarmIntent);
+			alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 6 * AlarmManager.INTERVAL_HOUR, alarmIntent);
 
+			preferences.edit().putBoolean(ALARM_SET_FLAG, true).apply();
+			
 			/*
-
 	        // Enable {@code SampleBootReceiver} to automatically restart the alarm when the
 	        // device is rebooted.
 	        ComponentName receiver = new ComponentName(context, SampleBootReceiver.class);
@@ -105,13 +104,11 @@ public class LoggerAlarmReceiver extends WakefulBroadcastReceiver {
 	        pm.setComponentEnabledSetting(receiver,
 	                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
 	                PackageManager.DONT_KILL_APP);
-
 			 */
-
+		}
+		else {
+			alarmManager.cancel(alarmIntent);
 		}
 	}
-
-
-
 
 }
