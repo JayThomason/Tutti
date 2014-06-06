727a19f0f2a326646c2a6057b098397eded9d4a3
JayThomason <jayt92@stanford.edu>
2014-06-05 18:37:23 -0700

    Updated javadoc for BootReceiver.

--- src/com/stanford/tutti/BootReceiver.java
+++ src/com/stanford/tutti/BootReceiver.java
@@ -3,11 +3,18 @@ package com.stanford.tutti;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+/**
+ * Receives BOOT_COMPLETED intent when the phone is turned on and resets the 
+ * logger alarm since turning the phone off disables all alarms.
+ */
 public class BootReceiver extends BroadcastReceiver {
 
-	LoggerAlarmReceiver alarm = new LoggerAlarmReceiver();
+	private LoggerAlarmReceiver alarm = new LoggerAlarmReceiver();
 
+	/**
+	 * (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED"))
