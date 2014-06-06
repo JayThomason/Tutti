93471856cbb602f5efa9e429d93eea82b3836423
JayThomason <jayt92@stanford.edu>
2014-06-05 18:15:38 -0700

    Updated javadoc for MainActivity and other minor changes.

--- src/com/stanford/tutti/MainActivity.java
+++ src/com/stanford/tutti/MainActivity.java
@@ -2,16 +2,10 @@ package com.stanford.tutti;
 
 import java.io.IOException;
 
-import org.apache.http.Header;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,9 +17,17 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/** 
+ * This activity displays the application's main menu when the app is booted.
+ */
 public class MainActivity extends Activity {
 	private Globals g;
 
+	/**
+	 * Loads the music from disk, resets the jam, sets the Welcome Text.
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,6 +39,12 @@ public class MainActivity extends Activity {
 		setWelcomeText();
 	}
 
+	/**
+	 * Stops logging a jam, resets the welcome text, and stops all discovery
+	 * related tasks.
+	 * 
+	 * @see android.app.Activity#onRestart()
+	 */
 	@Override
 	protected void onRestart() {
 		super.onRestart();
@@ -48,6 +56,10 @@ public class MainActivity extends Activity {
 		g.logger.endCurrentJam();
 	}
 
+	/**
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -55,6 +67,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -62,44 +78,75 @@ public class MainActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-			Intent intent = new Intent(MainActivity.this, SettingsMenuActivity.class); 
+			Intent intent = new Intent(MainActivity.this, 
+					SettingsMenuActivity.class); 
 			startActivity(intent); 
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
-
+	/**
+	 * Shows the name jam dialog so a new jam can be created. Binded to the
+	 * create jam button.
+	 * 
+	 * @param View view
+	 */
 	public void makeNewJam(View view) {
 		showNameJamDialog();
 	}
 
+	/**
+	 * Starts a new Join Jam activity. Bound to join local jam button.
+	 * 
+	 * @param View view
+	 */
 	public void joinJam(View view) {
 		Intent intent = new Intent(this, JoinJamActivity.class);
 		startActivity(intent);
 	}
 
+	/**
+	 * Starts a new SettingsMenuActivity. Bound to settings button.
+	 * 
+	 * @param View view
+	 */
 	public void settingsMenu(View view) {
 		Intent intent = new Intent(this, SettingsMenuActivity.class);
 		startActivity(intent);
 	}
 
+	/**
+	 * Starts a new HelpMenuActivity. Bound to help button.
+	 * 
+	 * @param View view
+	 */
 	public void helpMenu(View view) {
 		Intent intent = new Intent(this, HelpMenuActivity.class);
 		startActivity(intent);
 	}
 
+	/**
+	 * Starts a new thread that loads the music on the phone into a local 
+	 * database.
+	 */
 	private void loadLocalMusic() {
 		g.db.dropTable("songs"); 
 		g.localLoaderThread = new MusicLibraryLoaderThread(this);
 		g.localLoaderThread.start();	
 	}
 
+	/**
+	 * Drops the jam table to ensure no songs are preloaded in the jam.
+	 */
 	private void initializeJam() {
-		Globals g = (Globals) getApplicationContext(); 
 		g.db.dropTable("jam"); 
 	}
 
+	/* *
+	 * Sets the welcome text to either welcome the user by their username or to
+	 * ask the user to set his or her username in the settings.
+	 */
 	private void setWelcomeText() {
 		Globals g = (Globals) getApplicationContext(); 
 		TextView welcomeText = (TextView) findViewById(R.id.welcome_message); 
@@ -110,6 +157,10 @@ public class MainActivity extends Activity {
 		}
 	}
 
+	/**
+	 * Displays the name jam dialog on screen, allowing the user to create a
+	 * jam with a certain name.
+ 	 */
 	private void showNameJamDialog() {
 		final EditText input = new EditText(MainActivity.this);
 		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
@@ -131,12 +182,20 @@ public class MainActivity extends Activity {
 		    @Override
 		    public void onFocusChange(View v, boolean hasFocus) {
 		        if (hasFocus) {
-		            nameDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+		            nameDialog.getWindow().setSoftInputMode(
+		            		WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 		        }
 		    }
 		});
 	}
 
+	/**
+	 * Sets the listener for the name jam dialog. Starts the jam if the name
+	 * provided is valid.
+	 * 
+	 * @param nameDialog The name jam dialog being displayed
+	 * @param input The EditText from the name jam dialog input box
+	 */
 	private void setNameDialogShowListener(final AlertDialog nameDialog, final EditText input) {
 		nameDialog.setOnShowListener(new DialogInterface.OnShowListener() {
 			@Override
@@ -148,10 +207,11 @@ public class MainActivity extends Activity {
 						String jamName = input.getText().toString();
 						if (jamName.contains(" ")) {
 							Toast.makeText(getApplicationContext(), 
-									"The jam name may not contain spaces.", Toast.LENGTH_LONG).show();
+									"The jam name may not contain spaces.", 
+									Toast.LENGTH_LONG).show();
 						}
 						else {
-							startJam(jamName, nameDialog);
+							startJam(jamName);
 						}
 					}
 				});
@@ -159,8 +219,13 @@ public class MainActivity extends Activity {
 		});
 	}
 
-	// return true if jam created in db, false if definite failure
-	private boolean startJam(String jamName, final AlertDialog nameDialog) {
+	/**
+	 * Starts a new jam by booting a server, starting the jam broadcast,
+	 * 
+	 * @param jamName The name of the jam to be started
+	 * @returns True if jam created successfully and false on failure
+	 */
+	private boolean startJam(String jamName) {
 		try {
 			Server s = new Server(g);
 			s.start();
@@ -173,16 +238,22 @@ public class MainActivity extends Activity {
 		int port = g.getServerPort();
 		if (port > 0) {
 			startJamBroadcast(jamName);
-			//createJamInDatabase(jamName.isEmpty() ? null : jamName, nameDialog, port);
 			return true;
 		}
 		else {
-			Toast.makeText(this, "Unable to create jam in database -- bad port number!", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, 
+					"Unable to create jam in database -- bad port number!", 
+					Toast.LENGTH_SHORT).show();
 			return false;
 		}
 	}
 
-	// starts broadcasting the jam by making 
+	/**
+	 * Starts the master client ping thread, sets this phone as the jam master,
+	 * sets the jam name, makes the jam discoverable, starts a new logger, and 
+	 * 
+	 * @param jamName The name of the jam being started
+	 */
 	private void startJamBroadcast(String jamName) {
 		g.jam.startMasterClientPingThread();
 		g.jam.setMaster(true);
@@ -196,5 +267,4 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(MainActivity.this, BrowseMusicActivity.class);
 		startActivity(intent);
 	}
-
 }
